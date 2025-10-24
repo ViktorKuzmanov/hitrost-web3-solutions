ae40a10849f06061140d037273fda874f87509e6
Viktor Kuzmanov <viktor.kuzmanov@students.finki.ukim.mk>
2025-10-24 03:06:42 +0200

    vaa linija mi kaza da a update lovable ai

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/hitrost-web3-solutions/', // Add this line
   server: {
     host: "::",
     port: 8080,
